9bdf810537daa6163bf13693395202cfd7cf9dbe
flexTestr <200045432+flexTestr@users.noreply.github.com>
2026-08-18 14:47:45 +0530

    Create SampleWorkflowTest.java

---
+++ src/SampleWorkflowTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class SampleWorkflowTest {
+
+    @Test
+    public void testPluginRuns() {
+        assertTrue(true);
+    }
+}
